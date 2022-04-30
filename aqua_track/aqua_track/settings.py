9e540ccbba080bbfe3821db491527024dcaf1de9
Nicholas Craig <nick@Nicholass-MacBook-Pro.local>
2022-04-30 09:40:02 -0400

    Changes are minor updates to reflect new local settings and fixing sttempted fixes to not having local settings setup on new computer. Added ModHeader link to readme too.

--- aqua_track/aqua_track/settings.py
+++ aqua_track/aqua_track/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-
+SECRET_KEY = 'gYjMU_iMnJjdm3nSdE5SaFOtzqXFoOAzU5bdKpDFDYw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
